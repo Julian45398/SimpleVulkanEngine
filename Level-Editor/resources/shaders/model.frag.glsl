ef96c90d57505fdfb65874f4768553d09ea7e573
Julian Schreyer <schreyerjulian@googlemail.com>
2025-09-29 19:27:40 +0200

    updated shaders

--- Level-Editor/resources/shaders/model.frag.glsl
+++ Level-Editor/resources/shaders/model.frag.glsl
@@ -6,19 +6,33 @@
 
 layout(set = 1, binding = 0) uniform sampler texSampler;
 layout(set = 1, binding = 1) uniform texture2D textures[128];
-layout(push_constant) uniform PushConstants {
-    vec4 colorModifier;
+
+layout(push_constant) uniform Push {
+    mat4 model;
+    vec4 color;
 } pc;
 
+//layout(location = 0) in vec2 fragUV;
+//layout(location = 1) in float color;
+//layout(location = 2) flat in uint texIndex;
+
 layout(location = 0) in vec2 fragUV;
-layout(location = 1) in float color;
+layout(location = 1) in vec4 color;
 layout(location = 2) flat in uint texIndex;
+layout(location = 3) in float intensity;
+
+
 
 layout(location = 0) out vec4 outColor;
 
 void main() {
     //outColor = texture(sampler2D(textures[texIndex], texSampler), fragUV) * color * pc.colorModifier;
-    outColor = mix(texture(sampler2D(textures[texIndex], texSampler), fragUV) * color, pc.colorModifier, pc.colorModifier.a);
+    //color.r = 1.0;
+    //color.g = 1.0;
+    //color.b = 1.0;
+    //color.a = 1.0;
+    //vec4 color2 = vec4(1.0, 1.0, 1.0, 1.0);
+    outColor = mix(texture(sampler2D(textures[texIndex], texSampler), fragUV) * color * intensity, pc.color, pc.color.a);
     //outColor = texture(texSampler, uvFragCoord) * color;
     outColor[3] = 1.0;
     //outColor = vec4(color, 1.0);
